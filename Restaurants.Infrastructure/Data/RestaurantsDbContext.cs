340eebea86910630ec2ae478943b3838a40ea751
Mahmoud Saleh <57641037+Mahmooooud33@users.noreply.github.com>
2024-11-10 20:56:22 +0200

    3.1 Add Felixable Database connection

--- Restaurants.Infrastructure/Data/RestaurantsDbContext.cs
+++ Restaurants.Infrastructure/Data/RestaurantsDbContext.cs
@@ -3,15 +3,10 @@ using Restaurants.Domain.Entities;
 
 namespace Restaurants.Infrastructure.Data;
 
-internal class RestaurantsDbContext : DbContext
+internal class RestaurantsDbContext(DbContextOptions<RestaurantsDbContext> options) : DbContext(options)
 {
     internal DbSet<Restaurant> Restaurants { get; set; }
     internal DbSet<Dish> Dishes { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=RestaurantsDb;Trusted_Connection=True;TrustServerCertificate=True;");
-    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -25,5 +20,4 @@ internal class RestaurantsDbContext : DbContext
             .WithOne()
             .HasForeignKey(d => d.RestaurantId);
     }
-
 }
