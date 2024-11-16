d6949543823a5f1c31269f05847fed89459db20e
Mahmoud Saleh <57641037+Mahmooooud33@users.noreply.github.com>
2024-11-16 17:41:20 +0200

    4.8 Add Swagger docs

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -2,13 +2,13 @@ using Restaurants.Application.Extensions;
 using Restaurants.Infrastructure.Extensions;
 using Restaurants.Infrastructure.Seeders;
 using Serilog;
-using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
@@ -23,6 +23,12 @@ var seeder = scope.ServiceProvider.GetRequiredService<IRestaurantSeeder>();
 await seeder.Seed();
 // Configure the HTTP request pipeline.
 app.UseSerilogRequestLogging();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
