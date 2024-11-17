b50eb719ac9c32e0fa14c00a32fc9f358756a565
Mahmoud Saleh <57641037+Mahmooooud33@users.noreply.github.com>
2024-11-17 03:16:06 +0200

    6.1 Extract presentation layer services

---
+++ Restaurants.API/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.OpenApi.Models;
+using Restaurants.API.Middlewares;
+using Serilog;
+
+namespace Restaurants.API.Extensions;
+
+public static class WebApplicationBuilderExtensions
+{
+    public static void AddPresentationServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllers();
+
+        #region Swagger
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme
+            {
+                Type = SecuritySchemeType.Http,
+                Scheme = "Bearer"
+            });
+
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference{ Type = ReferenceType.SecurityScheme, Id = "bearerAuth" }
+                    },
+                    []
+                }
+            });
+        });
+        #endregion
+
+        builder.Services.AddEndpointsApiExplorer();
+
+        #region Custom Middlewares
+        builder.Services.AddScoped<ErrorHandlingMiddleware>();
+        builder.Services.AddScoped<RequestTimeLoggingMiddleware>();
+        #endregion
+
+        builder.Host.UseSerilog((context, configuration) =>
+            configuration.ReadFrom.Configuration(context.Configuration));
+    }
+}
--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.OpenApi.Models;
+using Restaurants.API.Extensions;
 using Restaurants.API.Middlewares;
 using Restaurants.Application.Extensions;
 using Restaurants.Domain.Entities;
@@ -10,43 +10,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-
-#region Swagger
-builder.Services.AddSwaggerGen(c =>
-{
-    c.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme
-    {
-        Type = SecuritySchemeType.Http,
-        Scheme = "Bearer"
-    });
-
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference{ Type = ReferenceType.SecurityScheme, Id = "bearerAuth" }
-            },
-            []
-        }
-    });
-});
-#endregion
-
-builder.Services.AddEndpointsApiExplorer();
-
-#region Custom Middlewares
-builder.Services.AddScoped<ErrorHandlingMiddleware>();
-builder.Services.AddScoped<RequestTimeLoggingMiddleware>();
-#endregion
+builder.AddPresentationServices();
 
 builder.Services.AddApplication();
 
 builder.Services.AddInfrastructure(builder.Configuration);
-
-builder.Host.UseSerilog((context, configuration) =>
-    configuration.ReadFrom.Configuration(context.Configuration));
 
 var app = builder.Build();
 
